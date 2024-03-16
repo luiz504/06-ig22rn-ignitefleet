6a5058843cf5bfe254877e4c4ffe44a934ca39db
luiz bueno <luiz504@hotmail.com>
2024-03-16 18:45:54 -0300

    chore: base project with assets/theme

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+import { styles } from './styles'
+
+export const SignIn: FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text> SignIn </Text>
+    </View>
+  )
+}
