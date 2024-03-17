5eb4c633af709e33fc0c0bf76668447b65b87021
luiz bueno <luiz504@hotmail.com>
2024-03-17 20:12:09 -0300

    feat: integrate atlas/realdb

--- App.tsx
+++ App.tsx
@@ -6,28 +6,38 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { QueryClientProvider } from '@tanstack/react-query'
-
-import { SignIn } from './src/screens/SignIn'
+import { AppProvider, UserProvider } from '@realm/react'
 
 import { theme } from '~/theme'
-import { Loading } from '~/components/Loading'
 
 import { queryClient } from '~/services/queryClient'
+import { REACT_APP_ID } from '@env'
+
+import { Loading } from '~/components/Loading'
+
+import { Home } from '~/screens/Home'
+import { SignIn } from '~/screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
-
+  console.log('dd', fontsLoaded)
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <StatusBar
-          barStyle={'light-content'}
-          backgroundColor={'transparent'}
-          translucent
-        />
-
-        {fontsLoaded ? <SignIn /> : <Loading />}
-      </ThemeProvider>
-    </QueryClientProvider>
+    <AppProvider id={REACT_APP_ID}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <StatusBar
+            barStyle={'light-content'}
+            backgroundColor={'transparent'}
+            translucent
+          />
+          {!fontsLoaded && <Loading />}
+          {fontsLoaded && (
+            <UserProvider fallback={<SignIn />}>
+              <Home />
+            </UserProvider>
+          )}
+        </ThemeProvider>
+      </QueryClientProvider>
+    </AppProvider>
   )
 }
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { Text, View } from 'react-native'
+
+// import styles from './styles'
+
+export const Home: FC = () => {
+  return (
+    <View>
+      <Text style={{ color: 'red' }}> Home </Text>
+    </View>
+  )
+}
