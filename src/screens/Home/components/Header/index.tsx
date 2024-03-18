1085f28ac3c22d1f83c451301b29253f2ebea71b
luiz bueno <luiz504@hotmail.com>
2024-03-17 21:31:17 -0300

    feat(Home): user Image

--- src/screens/Home/components/Header/index.tsx
+++ src/screens/Home/components/Header/index.tsx
@@ -1,14 +1,18 @@
-import { FC } from 'react'
+import React, { FC } from 'react'
 import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
 
-import { Container, Greeting, Message, Name } from './styles'
+import { Container, Greeting, Message, Name, Picture } from './styles'
 
 import { theme } from '~/theme'
 
 export const Header: FC = () => {
   return (
     <Container>
+      <Picture
+        source={{ uri: 'https://github.com/luiz504.png' }}
+        placeholder="L184i9offQof00ayfQay~qj[fQj["
+      />
       <Greeting>
         <Message>Hello,</Message>
 
