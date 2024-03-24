33ae8591a7b6ab45addd9524433449c27d7f3dae
luiz bueno <luiz504@hotmail.com>
2024-03-24 16:03:35 -0300

    feat: refactor inputs refs

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,10 @@
 import { FC, useRef } from 'react'
-import { ScrollView, TextInput } from 'react-native'
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+} from 'react-native'
 
 import { LicensePlateInput } from '~/screens/Departure/components/LicensePlateInput'
 import { PurposeCard } from '~/screens/Departure/components/PurposeCard'
@@ -7,6 +12,8 @@ import { Button } from '~/components/Button'
 import { Header } from '~/components/Header'
 
 import { Container, Body } from './styles'
+const KeyboardAvoidingViewBehavior =
+  Platform.OS === 'android' ? 'height' : 'position'
 export const DepartureScreen: FC = () => {
   const purposeRef = useRef<TextInput>(null)
 
@@ -16,30 +23,33 @@ export const DepartureScreen: FC = () => {
   return (
     <Container>
       <Header title="Departure" />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={KeyboardAvoidingViewBehavior}
+      >
+        <ScrollView>
+          <Body>
+            <LicensePlateInput
+              label={'License Plate'}
+              placeholder="BRA1234"
+              onSubmitEditing={() => purposeRef.current?.focus()}
+              returnKeyType="next"
+            />
+            <PurposeCard
+              label="Purpose"
+              ref={purposeRef}
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType="send"
+              blurOnSubmit
+            />
 
-      <ScrollView>
-        <Body>
-          <LicensePlateInput
-            label={'License Plate'}
-            placeholder="BRA1234"
-            onSubmitEditing={() => purposeRef.current?.focus()}
-            returnKeyType="next"
-          />
-
-          <PurposeCard
-            label="Purpose"
-            ref={purposeRef}
-            onSubmitEditing={handleDepartureRegister}
-            returnKeyType="send"
-            blurOnSubmit
-          />
-
-          <Button
-            label="Register Departure"
-            onPress={handleDepartureRegister}
-          />
-        </Body>
-      </ScrollView>
+            <Button
+              label="Register Departure"
+              onPress={handleDepartureRegister}
+            />
+          </Body>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
