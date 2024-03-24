644a61e4e0d292302a779fe5ab10bfbfe5b2368b
luiz bueno <luiz504@hotmail.com>
2024-03-24 19:10:59 -0300

    feat(arrival): base interface

--- src/screens/app/Arrival/index.tsx
+++ src/screens/app/Arrival/index.tsx
@@ -1,7 +1,33 @@
 import { FC } from 'react'
 
-import { Container } from './styles'
+import { Header } from '~/components/Header'
+import { Body, Container, Label, LicensePlate, Purpose, Footer } from './styles'
 
-export const Arrival: FC = () => {
-  return <Container />
+import { AppScreenProps } from '~/routes/app.routes'
+import { Button } from '~/components/Button'
+
+type Props = AppScreenProps<'arrival'>
+export const Arrival: FC<Props> = ({ route: { params } }) => {
+  const veihcleId = params.id
+  return (
+    <Container>
+      <Header title="Arrival" />
+
+      <Body>
+        <Label>License plate</Label>
+        <LicensePlate>vddud-12312</LicensePlate>
+
+        <Label>Purpose</Label>
+        <Purpose>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
+          numquam, illum accusantium, autem asperiores velit inventore
+          laudantium nihil quos officiis porro, libero magnam molestias. Id
+          molestias dolores quas nihil dolorum.
+        </Purpose>
+      </Body>
+      <Footer>
+        <Button label="Register Arrival" />
+      </Footer>
+    </Container>
+  )
 }
--- src/screens/app/Arrival/styles.ts
+++ src/screens/app/Arrival/styles.ts
@@ -2,5 +2,35 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_100};
+  background-color: ${({ theme }) => theme.COLORS.GRAY_800};
+`
+export const Body = styled.View`
+  flex: 1;
+  padding: 0 32px;
+`
+export const Label = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+
+  margin-top: 32px;
+  margin-bottom: 5px;
+`
+export const LicensePlate = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_100};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XXXL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
+`
+export const Purpose = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_100};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  text-align: justify;
+`
+export const Footer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  gap: 16px;
+  padding: 0 32px;
+  margin: 32px 0;
 `
