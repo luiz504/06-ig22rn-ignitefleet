1bf29be1bc5c685dd25640fba97e72a196fb50e6
luiz bueno <luiz504@hotmail.com>
2024-03-24 10:08:40 -0300

    chore: realm db first schema/generate

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,8 +1,33 @@
 /* eslint-disable no-use-before-define */
 import { Realm } from '@realm/react'
 import { ObjectSchema } from 'realm'
+
+type GenerateProps = {
+  user_id: string
+  license_plate: string
+  description: string
+}
 export class Historic extends Realm.Object<Historic> {
-  static generate() {}
+  _id!: string
+  user_id!: string
+  license_plate!: string
+  description!: string
+  status!: string
+  created_at!: string
+  updated_at!: string
+
+  static generate({ user_id, license_plate, description }: GenerateProps) {
+    return {
+      _id: new Realm.BSON.UUID(),
+      user_id,
+      license_plate,
+      description,
+      status: 'departure',
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
+  }
+
   static schema: ObjectSchema = {
     name: 'Historic',
     primaryKey: '_id',
