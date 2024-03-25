6a515e4f346abbb2c6d120773957cce690e2da3c
luiz bueno <luiz504@hotmail.com>
2024-03-25 09:09:42 -0300

    feat(arrival): useHistoricObject integration

--- src/screens/app/Arrival/index.tsx
+++ src/screens/app/Arrival/index.tsx
@@ -1,31 +1,36 @@
 import { FC } from 'react'
+import { X } from 'phosphor-react-native'
+import { BSON } from 'realm'
+
+import { Historic } from '~/libs/realm/schemas/Historic'
+import { useObject } from '~/libs/realm'
 
 import { Header } from '~/components/Header'
 import { Button } from '~/components/Button'
+import { ButtonIcon } from '~/components/ButtonIcon'
 import { Body, Container, Label, LicensePlate, Purpose, Footer } from './styles'
 
 import { AppScreenProps } from '~/routes/app.routes'
-import { X } from 'phosphor-react-native'
-import { ButtonIcon } from '~/components/ButtonIcon'
 
 type Props = AppScreenProps<'arrival'>
 export const Arrival: FC<Props> = ({ route: { params } }) => {
   const vehicleId = params.id
+
+  const historic = useObject(
+    Historic,
+    new BSON.UUID(vehicleId) as unknown as string,
+  )
+
   return (
     <Container>
       <Header title="Arrival" />
 
       <Body>
         <Label>License plate</Label>
-        <LicensePlate>vddud-12312</LicensePlate>
+        <LicensePlate>{historic?.license_plate}</LicensePlate>
 
         <Label>Purpose</Label>
-        <Purpose>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae
-          numquam, illum accusantium, autem asperiores velit inventore
-          laudantium nihil quos officiis porro, libero magnam molestias. Id
-          molestias dolores quas nihil dolorum.
-        </Purpose>
+        <Purpose>{historic?.description}</Purpose>
       </Body>
       <Footer>
         <ButtonIcon icon={X} />
